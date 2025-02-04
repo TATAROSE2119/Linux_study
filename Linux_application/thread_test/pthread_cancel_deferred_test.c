8dad49c796bc3c9994fb481d529cd87ee2c2eb47
TATAROSE_on_Ubuntu <181440507@qq.com>
2025-02-04 11:04:12 +0800

    添加线程取消的延迟测试

--- Linux_application/thread_test/pthread_cancel_deferred_test.c
+++ Linux_application/thread_test/pthread_cancel_deferred_test.c
@@ -4,3 +4,38 @@
 #include <unistd.h>
 
 
+void *task(void *arg)
+{
+    printf("Thread start\n");
+
+
+    printf("working\n");
+    sleep(5);
+    pthread_testcancel();
+    printf("After cancel point\n");
+
+    return NULL;
+}
+int main(){
+    pthread_t tid;
+    void *res;
+
+    // create thread
+    pthread_create(&tid, NULL, task, NULL);
+    // cancel son thread
+    if(pthread_cancel(tid) != 0){
+        perror("pthread_cancel");
+        exit(1);
+    }
+    // wait for son thread
+    pthread_join(tid, &res);
+    // check son thread return value
+    if(res == PTHREAD_CANCELED){
+        printf("son Thread was canceled\n");
+    }
+    else{
+        printf("son Thread was not canceled\n");
+    }
+    return 0;
+
+}
