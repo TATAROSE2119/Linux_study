1c6d55945eda920c0ea06bf1c7de9387e663f633
tatarose <1814480507@qq.com>
2025-05-27 11:28:08 +0800

    修复inet_endian_convert.c中的参数声明错误，确保argv指针正确传递

--- Linux_application/net/inet_endian_convert.c
+++ Linux_application/net/inet_endian_convert.c
@@ -7,7 +7,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-int main(int argc,char const argv[]){
+int main(int argc,char const *argv[]){
     printf("192.168.6.101  的16进制表示为:0x%x  0x%x 0x%x 0x%x\n",192,168,6,101);
     //声明结构体接受
     struct sockaddr_in server_addr;
